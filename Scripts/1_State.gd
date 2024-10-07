63836c87f2f5f7ba2188d7eba59c63587ffe3965
ChopsDev <101037256+ChopsDev@users.noreply.github.com>
2024-10-07 15:20:09 -0600

    Create 1_State.gd

---
+++ Scripts/1_State.gd
@@ -0,0 +1,18 @@
+# This script is important and is used to switch between states within the states themselves
+
+extends Node
+class_name CardState
+
+signal Transitioned
+
+func enter():
+	pass
+
+func exit():
+	pass
+
+func update(delta):
+	pass
+
+func physics_update(delta):
+	pass
